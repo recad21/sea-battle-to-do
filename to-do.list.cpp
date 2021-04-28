cd6e576523244e3467c13249c517172d16f7ac7b
IB1aZe <43469371+IB1aZe@users.noreply.github.com>
2021-04-28 21:43:57 +0300

    Add files via upload

--- to-do.list.cpp
+++ to-do.list.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <clocale>
 #include <cstdio>
-#include <stdio.h>
-#include <windows.h>
+#include <string>
 using namespace std;
 
 
@@ -19,32 +18,31 @@ void start(){
 }
 struct Do
 {
-	char* name[50];
-	char* opis[50];
+	string name;
+	string opis;
 	int prior;
 	int day;
 	int month;
 	int week;
 };
 
-Do* massDo = nullptr;
-Do* newMass = nullptr;
+
 int men = 0;
  
 int menu() {
+	cin.clear();
 	cout << "What you want to do?" << endl;
 	cout << "1. Add deal\n2. Edit deal\n3. Delete deal\n4. Display deal\n5. Search deal\n6. Exit" << endl;
 	cin >> men;
 	return men;
 }
 
-void create(int N) {
-	massDo = new Do;
+void create(int N, Do *massDo) {
 	for (int i = 0; i < N; i++) {
 		cout << "Enter name deal number " << i + 1 << ":\t";
-		cin >> *massDo[i].name;
-		cout << "Enter description deal number " << i + 1 << ":\t";
-		cin >> *massDo[i].opis;
+		cin >> massDo[i].name;
+		cout << "Enter description deal anumber " << i + 1 << ":\t";
+		cin >> massDo[i].opis;
 		cout << "Enter priority deal (1-5) number " << i + 1 << ":\t";
 		cin >> massDo[i].prior;
 		cout << "Enter day deal number " << i + 1 << ":\t";
@@ -56,42 +54,64 @@ void create(int N) {
 	}
 }
 
-void showDay(int N, int day) {
+void showDay(int N, int day, Do *massDo) {
 	for (int i = 0; i < N; i++) {
 		if (day == massDo[i].day) {
-			cout << "Day deal number " << (i + 1) << ": " << massDo[i].day << endl;
+			cout << "Name: " << massDo[i].name << endl;
+			cout << "Pescription: " << massDo[i].opis << endl;
+			cout << "Priority: " << massDo[i].prior << endl;
+			cout << "Day: " << massDo[i].day << endl;
+			cout << "Week: " << massDo[i].week << endl;
+			cout << "Month: " << massDo[i].month << endl;
 		}
 	}
 }
-void showMonth(int N, int month) {
+void showMonth(int N, int month, Do *massDo) {
 	for (int i = 0; i < N; i++) {
 		if (month == massDo[i].month) {
-			cout << "Month deal number " << (i + 1) << ": " << massDo[i].month << endl;
+			cout << "Name: " << massDo[i].name << endl;
+			cout << "Pescription: " << massDo[i].opis << endl;
+			cout << "Priority: " << massDo[i].prior << endl;
+			cout << "Day: " << massDo[i].day << endl;
+			cout << "Week: " << massDo[i].week << endl;
+			cout << "Month: " << massDo[i].month << endl;
 		}
 	}
 }
-void showWeek(int N, int week) {
+void showWeek(int N, int week, Do *massDo) {
 	for (int i = 0; i < N; i++) {
 		if (week == massDo[i].week) {
-			cout << "Week deal number " << (i + 1) << ": " << massDo[i].week << endl;
+			cout << "Name: " << massDo[i].name << endl;
+			cout << "Pescription: " << massDo[i].opis << endl;
+			cout << "Priority: " << massDo[i].prior << endl;
+			cout << "Day: " << massDo[i].day << endl;
+			cout << "Week: " << massDo[i].week << endl;
+			cout << "Month: " << massDo[i].month << endl;
 		}
 	}
 }
 
-void search(int N, int s) {
-	char des[50];
-	char name[50];
+void search(int N, int s, Do *massDo) {
+	string des;
+	string name;
 	int pr;
 	switch (s) {
 	case 1:
 		cout << "Enter name:" << endl;
 		cin >> name;
 		for (int i = 0; i < N; i++) {
-			if (name == massDo[i].name[50]) {
-				cout << "Name deal number " << (i + 1) << ": " << massDo[i].name << endl;
+			if (name == massDo[i].name)
+			{
+				cout << "Name: " << massDo[i].name << endl;
+				cout << "Pescription: " << massDo[i].opis << endl;
+				cout << "Priority: " << massDo[i].prior << endl;
+				cout << "Day: " << massDo[i].day << endl;
+				cout << "Week: " << massDo[i].week << endl;
+				cout << "Month: " << massDo[i].month << endl;
 			}
-			else {
-				cout << "No deal for this name";
+			else if (name == "\0")
+			{
+				cout << "Name not found!" << endl;
 			}
 		}
 		break;
@@ -99,11 +119,13 @@ void search(int N, int s) {
 		cout << "Enter priority:" << endl;
 		cin >> pr;
 		for (int i = 0; i < N; i++) {
-			if (&pr == &massDo[i].prior) {
-				cout << "Priority deal number " << (i + 1) << ": " << massDo[i].prior << endl;
-			}
-			else {
-				cout << "No deal for this priority";
+			if (pr == massDo[i].prior) {
+				cout << "Name: " << massDo[i].name << endl;
+				cout << "Pescription: " << massDo[i].opis << endl;
+				cout << "Priority: " << massDo[i].prior << endl;
+				cout << "Day: " << massDo[i].day << endl;
+				cout << "Week: " << massDo[i].week << endl;
+				cout << "Month: " << massDo[i].month << endl;
 			}
 		}
 		break;
@@ -111,66 +133,72 @@ void search(int N, int s) {
 		cout << "Enter descriprion:" << endl;
 		cin >> des;
 		for (int i = 0; i < N; i++) {
-			if (des == massDo[i].opis[50]) {
-				cout << "Description deal number " << (i + 1) << ": " << massDo[i].opis << endl;
+			if (des == massDo[i].opis) {
+				cout << "Name: " << massDo[i].name << endl;
+				cout << "Pescription: " << massDo[i].opis << endl;
+				cout << "Priority: " << massDo[i].prior << endl;
+				cout << "Day: " << massDo[i].day << endl;
+				cout << "Week: " << massDo[i].week << endl;
+				cout << "Month: " << massDo[i].month << endl;
 			}
-			else {
-				cout << "No deal for this descroption";
+			else if(des == "\0")
+			{
+				cout << "Description not found!" << endl;
 			}
 		}
 		break;
 	}
 }
 
-void changeName(char* newName, int N) {
-	int num;
-	cin >> num;
-	*newMass[num].name = newName;
+void changeName(string newName, int N, Do *massDo) {
+	massDo[N].name = newName;
 }
-void changeOpis(char* newOpis, int N) {
-	int num;
-	cin >> num;
-	*newMass[num].opis = newOpis;
+void changeOpis(string newOpis, int N, Do *massDo) {
+	massDo[N].opis = newOpis;
 }
-void changePrior(int newPrior, int N) {
+void changePrior(int newPrior, int N, Do *massDo) {
 	int pr;
 	do {
 		cin >> pr;
-		if (pr < 0 || pr >= N)
-		cout << "Invalid num. Reenter: ";
+		if (pr < 0 || pr >= N) {
+			cout << "Invalid num. Reenter: ";
+		}
 	} while (pr < 0 || pr >= N);
 	massDo[pr].prior = newPrior;
 }
-void changeDay(int newDay, int N) {
+void changeDay(int newDay, int N, Do *massDo) {
 	int day;
 	do {
 		cin >> day;
-		if (day < 0 || day >= N)
-		cout << "Invalid day. Reenter: ";
+		if (day < 0 || day >= N) {
+			cout << "Invalid day. Reenter: ";
+		}
 	} while (day < 0 || day >= N);
 	massDo[day].day = newDay;
 }
-void changeMonth(int newMonth, int N) {
+void changeMonth(int newMonth, int N, Do *massDo) {
 	int month;
 	do {
 		cin >> month;
-		if (month < 0 || month >= N)
-		cout << "Invalid month. Reenter: ";
+		if (month < 0 || month >= N) {
+			cout << "Invalid month. Reenter: ";
+		}
 	} while (month < 0 || month >= N);
 	massDo[month].month = newMonth;
 }
-void changeWeek(int newWeek, int N) {
+void changeWeek(int newWeek, int N, Do *massDo) {
 	int week;
 	do {
 		cin >> week;
-		if (week < 0 || week >= N)
-		cout << "Invalid num. Reenter: ";
+		if (week < 0 || week >= N) {
+			cout << "Invalid num. Reenter: ";
+		}
 	} while (week < 0 || week >= N);
 	massDo[week].week = newWeek;
 }
 
 
-void delDo(int N) {
+void delDo(int N, Do* massDo) {
 	int toDel;
 	cout << "Enter num deal to delete: ";
 	do {
@@ -198,11 +226,10 @@ void delDo(int N) {
 }
 
 int main() {
-	char name1[50], opis[50];
-	int pri, d, m, w;
+	string name1, opis;
+	int pri, d, m, w, s;
 	int N, ch, day, month, week, sh;
 	
-	int s, pr;
 	start();
 	cout << "Enter num deal: \t";
 	cin >> N;
@@ -211,12 +238,13 @@ int main() {
 		cin.ignore(7, '\n');
 		cin >> N;
 	}
-	char name[50];
-	//menu();
+	Do* massDo = new Do[N];
+	string name;
+	menu();
 	do {
 		switch (men) {
 		case 1:
-			create(N);
+			create(N, massDo);
 			menu();
 			break;
 		case 2:
@@ -228,39 +256,45 @@ int main() {
 				cin >> ch;
 			}
 			if (ch == 1) {
-				cout << "Enter new name: " << endl;
-				cin >> name1;
-				changeName(name1, N);
+				cout << "Enter name who need change:" << endl;
+				cin >> name;
+				for (int i = 0; i < N; i++) {
+					if (name == massDo[i].name) {
+						cout << "Enter new name: " << endl;
+						cin >> name1;
+						changeName(name1, i, massDo);
+					}
+				}
 			}
 			if (ch == 2) {
 				cout << "Enter new description: " << endl;
 				cin >> opis;
-				changeOpis(opis, N);
+				changeOpis(opis, N, massDo);
 			}
 			if (ch == 3) {
 				cout << "Enter new priority: " << endl;
 				cin >> pri;
-				changePrior(pri, N);
+				changePrior(pri, N, massDo);
 			}
 			if (ch == 4) {
 				cout << "Enter new day: " << endl;
 				cin >> d;
-				changeDay(d, N);
+				changeDay(d, N, massDo);
 			}
 			if (ch == 5) {
 				cout << "Enter new month: " << endl;
 				cin >> m;
-				changeMonth(m, N);
+				changeMonth(m, N, massDo);
 			}
 			if (ch == 6) {
 				cout << "Enter new week: " << endl;
 				cin >> w;
-				changeWeek(w, N);
+				changeWeek(w, N, massDo);
 			}
 			menu();
 			break;
 		case 3:
-			delDo(N);
+			delDo(N, massDo);
 			menu();
 			break;
 		case 4:
@@ -269,32 +303,28 @@ int main() {
 			if (sh == 1) {
 				cout << "Enter day:" << endl;
 				cin >> day;
-				showDay(N, day);
+				showDay(N, day, massDo);
 			}
 			else if (sh == 2) {
 				cout << "Enter month:" << endl;
 				cin >> month;
-				showMonth(N, month);
+				showMonth(N, month, massDo);
 			}
 			else if (sh == 3) {
 				cout << "Enter week:" << endl;
 				cin >> week;
-				showWeek(N, week);
+				showWeek(N, week, massDo);
 			}
 			menu();
 			break;
 		case 5:
-			
 			cout << "What do you want to find?\n1)Name\n2)Priority\n3)Description" << endl;
 			cin >> s;
-			//search(N, s);
+			search(N, s, massDo);
 			menu();
 			break;
 		case 6:
-			break;
-		default:
-			menu();
-			break;
+			return 0;
 		}
-	} while (men != 5);
+	} while (men);
 }
